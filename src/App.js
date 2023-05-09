657d50f67d46cbcd8c92c5bc5b331a2242c7c0a5
h3c70rdelAn9el <hectordelangel@gmail.com>
2023-05-09 14:51:22 -0700

    make the emojicontextprovider component wrap around the app component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,11 @@
-import React, { useState } from 'react'
+import React from 'react'
 import EmojiList from './components/EmojiList'
-import EmojiContextProvider from './components/EmojiContextProvider'
 
 function App() {
-    // const [emoji, setEmoji] = useState([])
-    // console.log(emoji)
     return (
         <div className="App">
             <h1 className="text-3xl text-center p-2">Emojis &#128512;</h1>
-            {/* <EmojiContextProvider value={{ emoji: [], setEmoji: () => {} }}> */}
-            <EmojiContextProvider>
-                <EmojiList />
-            </EmojiContextProvider>
+            <EmojiList />
         </div>
     )
 }
