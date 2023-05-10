dc7d92e68a91724afaeac2e07136d3bda3f809d6
h3c70rdelAn9el <hectordelangel@gmail.com>
2023-05-10 13:51:18 -0700

    add a link for github repo

--- src/App.js
+++ src/App.js
@@ -6,6 +6,13 @@ function App() {
         <div className="h-screen bg-gray-200 App">
             <h1 className="p-2 text-3xl text-center">Emojis &#128512;</h1>
             <EmojiList />
+            <a
+                href="https://github.com/h3c70rdelAn9el/my-react-3moji"
+                target="_blank"
+                rel="noreferrer"
+                className="fixed text-xs bottom-1 left-10">
+                Github Repo
+            </a>
         </div>
     )
 }
