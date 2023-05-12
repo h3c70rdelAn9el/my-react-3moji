88f4a6670a77320136369a43b9715f6312995133
h3c70rdelAn9el <hectordelangel@gmail.com>
2023-05-12 15:02:02 -0700

    make search component

--- src/components/EmojiList.jsx
+++ src/components/EmojiList.jsx
@@ -1,16 +1,17 @@
-import React, { useContext, useMemo, useState, useRef } from 'react'
+import React, { useContext, useMemo, useState } from 'react'
 import { EmojiContext } from './EmojiContextProvider.js'
+import EmojiSearch from './EmojiSearch.jsx'
 
 function EmojiList() {
     const { emoji } = useContext(EmojiContext)
-    const searchRef = useRef('')
-    const [search, setSearch] = useState(searchRef.current)
+    const [search, setSearch] = useState('')
 
     const filteredEmoji = useMemo(() => {
         const searchTerms = search
             .toLowerCase()
             .split(' ')
             .filter((term) => term.trim() !== '')
+
         return emoji.filter((emoji) => {
             return searchTerms.every(
                 (term) =>
@@ -20,24 +21,11 @@ function EmojiList() {
             )
         })
     }, [search, emoji])
+    console.log(filteredEmoji)
 
     return (
         <div className="relative w-5/6 max-w-5xl mx-auto overflow-scroll bg-gray-500 rounded-md shadow-lg emoji-list shadow-purple-500 h-3/4">
-            <div className="sticky top-0 flex flex-row items-center justify-center gap-4 p-3">
-                <input
-                    type="text"
-                    placeholder="Search for your emoji - by name, category, or HTML code"
-                    className="hidden w-5/6 p-2 text-sm bg-purple-200 border border-purple-500 rounded-md focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent md:block"
-                    onChange={(e) => setSearch(e.target.value)}
-                />
-                <input
-                    type="text"
-                    placeholder="Search by name, category, or HTML code"
-                    className="block w-5/6 p-2 text-xs bg-purple-200 border border-purple-500 rounded-md focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent md:hidden"
-                    onChange={(e) => setSearch(e.target.value)}
-                />
-            </div>
-
+            <EmojiSearch setSearch={setSearch} />
             <p className="ml-16 text-sm text-gray-800">
                 {filteredEmoji.length === 0
                     ? 'No emojis found'
